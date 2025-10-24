48d572fccc5006d46a9ab96744334517ff15a288
Andrés Lazcano <andreslazcano@MacBook-Pro-2.local>
2025-10-24 18:41:52 -0300

    Fix: Remover import de exports.py eliminado
    - Eliminar referencia a export_manager en src/utils/__init__.py
    - exports.py fue eliminado en la limpieza anterior
    - Mantener solo auth_manager y helpers en utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,7 +3,6 @@ Utilidades generales
 """
 
 from .auth import auth_manager
-from .exports import export_manager
 from .helpers import format_number_chilean, format_chilean, clean_text
 
-__all__ = ['auth_manager', 'export_manager', 'format_number_chilean', 'format_chilean', 'clean_text']
+__all__ = ['auth_manager', 'format_number_chilean', 'format_chilean', 'clean_text']
